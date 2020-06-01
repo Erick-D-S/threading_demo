74cc01445c8c2a209c91308e3e9a2e0086730ada
Erick-D-S <65356418+Erick-D-S@users.noreply.github.com>
2020-06-01 15:24:31 -0500

    fixed spacing.

--- semaphoreDemo.c
+++ semaphoreDemo.c
@@ -16,23 +16,23 @@ void* task_body(void* pv)
 		printf("%s--%d\n",ps,i);
 		sleep(1);
 	}
-    long status = 10;
+	long status = 10;
 	sem_post(&mutex); 
 	pthread_exit((void*)status);
 	
 }
 int main()
 {
-    void* status;
+	void* status;
 	sem_init(&mutex, 0, 1); 
 	pthread_t pt1,pt2,pt3;	//thread handle
 	pthread_create(&pt1,NULL,task_body,"A1");
 	pthread_create(&pt2,NULL,task_body,"B2");
-    pthread_create(&pt3,NULL,task_body,"C3");
+	pthread_create(&pt3,NULL,task_body,"C3");
 	pthread_join(pt1,&status);
 	pthread_join(pt2,NULL);
-    pthread_join(pt3,NULL);
-    printf("thread exit status is :%ld\n", (long)status);
+	pthread_join(pt3,NULL);
+    	printf("thread exit status is :%ld\n", (long)status);
 	printf("main--thank you\n");
 	sem_destroy(&mutex); 
 	return 0;	//exit(0);
